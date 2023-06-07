cfc4ac3bd0d5642719e5ae301eebef4c7fd57328
MikhaChe <feeltimenow@gmail.com>
2023-06-07 16:34:53 +0600

    trial bot

---
+++ index.js
@@ -0,0 +1,62 @@
+const tlgKey = require('node-telegram-bot-api');
+
+
+const botToken = '6105575827:AAG6657shRVQ_0JLeEQTups4G_ftD3wPvsw';
+const chatID = '-1001954148855'
+
+const bot = new tlgKey(botToken, {polling: true});
+
+const buttonsTablet = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{text: 'Reg', callback_data: 'subs'}, {text: 'Unreg', callback_data: 'unsubs'}]
+    ]
+  })
+}
+
+const subscribeFunc = async (msg) => {
+  console.log(msg);
+  const textMsg = msg.data;
+  const chatUserID = msg.message.chat.id;
+  const nameUser = msg.message.chat.first_name + msg.message.chat.last_name;
+  const text = textMsg + 'cribe!';
+  await bot.sendMessage(chatUserID, `You are ${text}`);
+  await bot.sendMessage(chatID, "User: " + nameUser +  " are " + text);
+}
+
+const startBot = () => {
+  bot.setMyCommands([
+    {command: '/start', description: 'Run'},
+    {command: '/info', description: 'Get info'},
+    {command: '/registration', description: 'Registration'}
+  ])
+  
+  bot.on('message', async msg => {
+    const textMsg = msg.text;
+    const chatUserID = msg.chat.id;
+    
+    if(textMsg === '/start') {
+      await bot.sendMessage(chatID, "I was turned on by the user: " + msg.from.first_name + msg.from.last_name);
+      await bot.sendSticker(chatUserID, 'https://tlgrm.ru/_/stickers/7bd/93c/7bd93cd5-8b8e-3cf7-ab8e-a3a15ebe0e33/9.webp');
+      return bot.sendMessage(chatUserID, "Hi, " + msg.from.first_name);
+    }
+  
+    if(textMsg === '/info') {
+      
+      await bot.sendSticker(chatUserID, 'https://tlgrm.ru/_/stickers/7bd/93c/7bd93cd5-8b8e-3cf7-ab8e-a3a15ebe0e33/4.webp');
+      return bot.sendMessage(chatUserID, "So far, I don't know why I was created");
+    }
+
+    if(textMsg === '/registration') {
+      return bot.sendMessage(chatUserID, "Subscribe", buttonsTablet); 
+    }
+  
+    return bot.sendMessage(chatUserID, 'I don\'t understand you, let\'s try again...')
+  })
+
+  bot.on('callback_query', msg => {
+    return subscribeFunc(msg);
+  })
+}
+
+startBot()
